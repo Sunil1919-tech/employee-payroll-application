2d5751ba7004f31ea243457fa4ca9a9aef1d2077
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-12-06 21:11:57 +0530

    Added: Created Employee Payroll Application with required dependencies

---
+++ src/main/java/com/bridgelabz/employeepayrollserviceapp/EmployeePayrollServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayrollserviceapp/EmployeePayrollServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
